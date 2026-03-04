3478b45b73a841471b76474f8c1f49996d5ecfdd
Ardikaas <ardikaajisetiawan@gmail.com>
2026-03-04 12:41:17 +0700

    fix minor bug

--- src/models/Equipment.js
+++ src/models/Equipment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
+const sequelize = require('../config/database');
 
 const Equipment = sequelize.define('Equipment', {
   equipmentId:       { type: DataTypes.STRING(20),  primaryKey: true,  field: 'equipment_id' },
