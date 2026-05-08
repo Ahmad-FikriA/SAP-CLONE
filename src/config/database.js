dc2db6a87dd59e913c115e69fd277c26afaedb25
Ardikaas <ardikaajisetiawan@gmail.com>
2026-05-08 08:41:15 +0700

    fix: override sequelize datetime format to omit timezone for sql server

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// Mencegah error format datetime di SQL Server dengan menghapus timezone offset (+07:00 dll)
+Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
+  date = this._applyTimezone(date, options);
+  return date.format('YYYY-MM-DD HH:mm:ss.SSS');
+};
+
 if (!process.env.URI) {
   console.error('ERROR: Database URI is not defined in .env');
   process.exit(1);
